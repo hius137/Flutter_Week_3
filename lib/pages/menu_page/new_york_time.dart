51ba1f826c740386b7dd276408ee1ca9a9db5c76
Minh Hieu <106766602+hius137@users.noreply.github.com>
2023-09-12 17:02:55 +0700

    update

--- lib/pages/menu_page/new_york_time.dart
+++ lib/pages/menu_page/new_york_time.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class NewYorkTime extends StatelessWidget {
   const NewYorkTime({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
